58a4b94161b1c0938b5b49595766c642f726b3ba
Kali <imokachi7@gmail.com>
2020-04-13 13:24:18 +0100

    express static files

--- xpress/app.js
+++ xpress/app.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 
@@ -7,8 +8,14 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+//morgan logs header infos into the console
+app.use(morgan('dev'));
+
+//serves up my html files in the pubic folder of express
+app.use(express.static(__dirname+'/public'));
+
 app.use((req, res, next)=>{
-    console.log(req.headers);
+    //console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('content-type', 'text/html');
     res.end('<html><body><h1>THis is a html page</h1></body></html>');
